4674eba7a8d1ff8db473681042303f42756e1c58
KDS-Limitless <limitlesskds@gmail.com>
2021-04-19 08:54:30 +0100

    indented json posts

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
     .then(text => {
-      let data = JSON.stringify(text)
-      fs.writeFile('result/post.json', data, function(err){
+      let data = JSON.stringify(text, null, 2)
+      fs.writeFile('result/posts.json', data, function(err){
         if (err) throw err;
         console.log("saved")
       });
